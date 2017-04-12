7f0adcff324b09418eb3f56f59854a6087e21ecf
Pierre-Victor Chaumier <chaumierpv@gmail.com>
2017-04-13 00:48:54 +0200

    Add endpoints for creating messages and reading from them

--- passage_backend/passage_backend/message/urls.py
+++ passage_backend/passage_backend/message/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^messages$', views.MessageList.as_view(), name='index'),
 ]
--- passage_backend/passage_backend/message/views.py
+++ passage_backend/passage_backend/message/views.py
@@ -6,9 +6,15 @@ from .serializers import MessageSerializer
 
 
 def index(request):
+    print(request.META['REMOTE_ADDR'])
     return JsonResponse({'conversation': 'test'})
 
 
-class SnippetList(generics.ListCreateAPIView):
+class MessageList(generics.ListCreateAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        # one still need to provide an IP field in the JSON but it is 
+        # actually set here
+        serializer.save(ip=self.request.META.get('REMOTE_ADDR', '0'))
