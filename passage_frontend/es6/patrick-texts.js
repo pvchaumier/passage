81281ef8762f01e6f825d31b59f3decb2f6c007d
mcoenca <m.coenca@gmail.com>
2017-04-22 18:36:18 +0200

    refactoring

---
+++ passage_frontend/es6/patrick-texts.js
@@ -0,0 +1,71 @@
+export const texts = [
+  {
+    text: 'salut'
+  },
+  {
+    text: '__input__1'
+  },
+  {
+    text: 'dis moi'
+  },
+  {
+    text: "j'ai rien a ecouter la"
+  },
+  {
+    text: "tu m'avais parle d'un groupe"
+  },
+  {
+    text: "c'etait quoi deja ?"
+  },
+  {
+    text: '__input__2'
+  },
+  {
+    text: 'Hm ha oui.'
+  },
+  {
+    text: "C'est cool."
+  },
+  {
+    text: 'ca va toi sinon ? depuis la derniere fois ...mes conseils ont été utiles ?'
+  },
+  {
+    text: 'en vrai'
+  },
+  {
+    text: "j'ai vu les photos sur facebook... t'avais l'air en forme dessus. ca m'a fait plaisir."
+  },
+  {
+    text: ';)'
+  },
+  {
+    text: 'enfin. je connais ces moments tsais'
+  },
+  {
+    text: 'dur de communiquer'
+  },
+  {
+    text: 'ds le brouillard'
+  },
+  {
+    text: 'peurs irrationnelles'
+  },
+  {
+    text: 'bref.'
+  },
+  {
+    text: "d'ailleurs"
+  },
+  {
+    text: "t'as pas 2 minutes la ? J'ai besoin de ton avis sur un truc un peu perso"
+  },
+  {
+    text: '__input__3'
+  },
+  {
+    text: 'cool merci. je te fais confiance pour garder ca pour toi hein.'
+  },
+  {
+    text: "d'ailleurs"
+  }
+]
